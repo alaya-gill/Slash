36b0b8107ec957b5f0a2112104d4b35f77bdfbd2
Ailyah <achristopher@ssidecisions.com>
2023-04-01 02:43:03 +0500

    change file opening

--- slash_django/slash/utils.py
+++ slash_django/slash/utils.py
@@ -4,15 +4,17 @@ import re
 def save_queries(queries):
     filename = queries[0].get('query')
     filename = re.sub(r'\W+', '', filename)
-    with open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(filename.lower()), 'w', encoding='utf-8') as fout:
-        json.dump(queries, fout)
+    fout = open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(filename.lower()), 'w', encoding='utf-8')
+    json.dump(queries, fout)
+    fout.close()
         
 def fetch_chat(convo):
     chat = []
     try:
         filename = re.sub(r'\W+', '', convo)
-        with open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(filename.lower()), 'r', encoding='utf-8') as fout:
-            chat = json.loads(fout.read())
+        fout=open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(filename.lower()), 'r', encoding='utf-8')
+        chat = json.loads(fout.read())
+        fout.close()
     except OSError as e:
         pass
     return chat
