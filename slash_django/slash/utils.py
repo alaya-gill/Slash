6a614c7a640f2748429c84c043b414a6ad8d2a74
Ailyah <achristopher@ssidecisions.com>
2023-04-01 23:24:33 +0500

    Deploy with file_exists

--- slash_django/slash/utils.py
+++ slash_django/slash/utils.py
@@ -1,27 +1,23 @@
-import json
+import json, os
 import re
 
 def save_queries(uuid, queries):
     # filename = queries[0].get('query')
     # filename = re.sub(r'\W+', '', filename)
     print("save_queries", uuid)
-    try:
-        with open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(uuid), 'w+', encoding='utf-8') as fout:
-            fout.seek(0)
-            file_data =  fout.read()
-            if len(file_data)==0 or file_data is None:
-                with open('./slash_django/utils/conversations/{}.json'.format(uuid), 'w', encoding='utf-8') as fout:
-                    json.dump(queries, fout)
-            else:
-                json_data = json.load(file_data)
-            
-                json_data += queries
-                fout.seek(0)
-                json.dump(json_data, fout, indent=4)
-    except FileNotFoundError as e:
-        with open('./slash_django/utils/conversations/{}.json'.format(uuid), 'w', encoding='utf-8') as fout:
-            json.dump(queries, fout)
-    # fout.close()
+    file_exists = os.path.exists('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(uuid))
+    if not file_exists:
+        f = open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(uuid), 'w', encoding='utf-8')
+        json.dump(queries, f)
+    else:
+        fout = open('./slash-django/Slash/slash_django/utils/conversations/{}.json'.format(uuid), 'r+', encoding='utf-8')
+        fout.seek(0)
+        file_data =  fout
+        json_data = json.load(file_data)
+        
+        json_data += queries
+        fout.seek(0)
+        json.dump(json_data, fout, indent=4)
         
 def fetch_chat(convo):
     print("fetch_chat",convo)
