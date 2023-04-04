39b471c2d8b807d79375aa30e6b45459cd2794cc
Ailyah <achristopher@ssidecisions.com>
2023-04-05 00:11:24 +0500

    Prototype with specified UI modifications

--- slash_django/slash/templatetags/space_to_hyphen.py
+++ slash_django/slash/templatetags/space_to_hyphen.py
@@ -1,6 +1,30 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+
 
 register = template.Library()
+
+@register.filter(name='cut')
+@stringfilter
 def cut(value, arg):
     """Removes all values of arg from the given string"""
     return value.replace(arg, '-')
+
+@register.filter(name='unique_id')
+@stringfilter
+def unique_id(value, arg):
+    """Removes all values of arg from the given string"""
+    return value.replace(arg, '#')
+
+@register.filter(name='id_a')
+@stringfilter
+def id_a(value, arg):
+    """Removes all values of arg from the given string"""
+    value = 'a#' + value + 'a#'
+    return value.replace(arg, 'a#')
+
+@register.filter(name='id_p')
+@stringfilter
+def id_p(value, arg):
+    """Removes all values of arg from the given string"""
+    return value.replace(arg, 'p#')
--- slash_django/slash/views.py
+++ slash_django/slash/views.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.shortcuts import render
@@ -11,18 +12,17 @@ from django.conf import settings
 class Slash:
     def __init__(self) -> None:
         self.queries = []
-        
+
     def get_index(self, request):
         print("index", is_ajax(request))
         length_of_chat = 0
         self.queries = []
         csrf = get_token(request)
         level_1, level_2, level_3 = self.get_levels_data()
-        context = {"csrf": csrf, "prompt": None, "query": "Search Your Query", "data": {
+        context = {"csrf": csrf, "prompt": None, "query": "/// Search Your Query", "data": {
             "level_1": level_1, "level_2": level_2, "level_3": level_3}, "chat": [], "length_of_chat": length_of_chat}
 
         return render(request, 'pages/chat.html', context=context)
-
 
     def get_json(self):
         f = open(settings.ROOT_FILE_PATH + '/slash_django/utils/models.json')
@@ -32,7 +32,6 @@ class Slash:
         data = json.load(f)
         return data
 
-
     def get_levels_data(self):
         models = self.get_json()
         level_1 = [obj for obj in models if obj.get('level') == 1]
@@ -40,13 +39,12 @@ class Slash:
         level_3 = [obj for obj in models if obj.get('level') == 3]
         return level_1, level_2, level_3
 
-
     """
     API triggered when user submit a query in a search box. 
     """
 
-
     def query(self, request):
+        time_now = datetime.now
         self.queries = []
         print(self.queries)
         print("query", is_ajax(request))
@@ -62,23 +60,25 @@ class Slash:
                 self.queries.append({
                     "query": prompt,
                     'prompt': response,
+                    'time': time_now().strftime('%H:%M')
                 })
                 context = {
                     "query": prompt,
                     'prompt': response,
+                    'time': time_now().strftime('%H:%M'),
                     "data": {"level_1": level_1, "level_2": level_2, "level_3": level_3},
                     "table-data": self.queries,
                 }
-                
+
                 print(self.queries)
                 save_queries(uuid, self.queries)
 
         return JsonResponse(context, status=200)
 
-
     def conversation(self, request, convo):
+        time_now = datetime.now
         print("conversation", is_ajax(request))
-        print("conversation",convo)
+        print("conversation", convo)
         self.queries = []
         length_of_chat = 0
         chat = []
@@ -86,7 +86,8 @@ class Slash:
             chat = fetch_chat(convo=convo)
         csrf = get_token(request)
         level_1, level_2, level_3 = self.get_levels_data()
-        context = {"csrf": csrf, "prompt": None, "query": "Search Your Query", "data": {
-            "level_1": level_1, "level_2": level_2, "level_3": level_3}, "chat": chat, "length_of_chat": len(chat) if chat  else length_of_chat}
+        context = {"csrf": csrf, "prompt": None, "query": "/// Search Your Query", "data": {
+            "level_1": level_1, "level_2": level_2, "level_3": level_3}, "chat": chat, 
+                   "time": time_now().strftime('%H:%M'),"length_of_chat": len(chat) if chat else length_of_chat}
         self.queries = chat
         return render(request, 'pages/chat.html', context=context)
